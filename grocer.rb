24ae247112c5790cf23fc86431e52580214d2cf2
Ruel Tingling <rvtingling@yahoo.com>
2019-09-16 13:20:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,22 @@
 def consolidate_cart(cart)
-  # code here
+  counter = 0 
+  consolidate_cart = 
+  [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+while consolidate_cart [counter] do 
+  puts consolidate_cart[counter]
+  counter += 1 
+end 
+
+consolidate_cart.each do |item, price|
+  puts "#{item}; #{price}"
 end
+
+
+
 
 def apply_coupons(cart, coupons)
   # code here
